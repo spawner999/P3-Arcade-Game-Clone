bde51c802d0e11c3e2434d0703b35fb1ce6508ad
spawner999 <lore.ferrario@gmail.com>
2015-09-29 11:23:03 +0200

    Power ups add, code refactor to match oojs specifics

--- js/game.js
+++ js/game.js
@@ -1,21 +1,31 @@
-//Game Object
+/*
+*
+*GAME OBJECT
+*
+*/
 var game = new Phaser.Game(800,600, Phaser.AUTO, 'phaser-demo', {preload: preload, create: create, update: update, render: render});
 
-//Variable Declarations
+/*
+*
+*VARIABLES
+*
+*/
 var cursors;
-var fire_button;
+var fireButton;
 var laser;
-var enemyLaser;
 var score = 0;
 var scoreText;
 var shields;
 var gameOver;
 var enemyTimer;
 var ufoTimer;
-var BULLET_TIMER = 0;
-var BULLET_SPEED = 150;
-var BULLET_SPACING = 600;
-var SHIP_SPEED = 220;
+var powerTimer;
+var bulletTimer = 0;
+var textOptions = {
+		font: '32px Arial',
+		fill: '#ffffff',
+		align: 'center'
+};
 
 /*
 *
@@ -23,9 +33,10 @@ var SHIP_SPEED = 220;
 *
 */
 
-//Player entity
+//Player class
 var Player = function(game){
 	this.health = 100;
+	this.SHIP_SPEED = 220;
 	Phaser.Sprite.call(this, game, 0, 300, 'ship');
 	this.anchor.setTo(-0.25, 0);
 	game.add.existing(this);
@@ -33,21 +44,20 @@ var Player = function(game){
 };
 //Extends Phaser's Sprite class
 Player.prototype = Object.create(Phaser.Sprite.prototype);
-Player.prototype.constructor = Player;
 //This method handles input and updates player position and actions
 Player.prototype.update = function(){
 	this.body.velocity.setTo(0,0);
 	if(cursors.up.isDown){
-		this.body.velocity.y = -SHIP_SPEED;
+		this.body.velocity.y = -this.SHIP_SPEED;
 	}
 	if(cursors.down.isDown){
-		this.body.velocity.y = SHIP_SPEED;
+		this.body.velocity.y = this.SHIP_SPEED;
 	}
 	if(cursors.left.isDown){
-		this.body.velocity.x = -SHIP_SPEED;
+		this.body.velocity.x = -this.SHIP_SPEED;
 	}
 	if(cursors.right.isDown){
-		this.body.velocity.x = SHIP_SPEED;
+		this.body.velocity.x = this.SHIP_SPEED;
 	}
 	if(this.y > game.height -105){
 		this.y = game.height - 105;
@@ -61,14 +71,16 @@ Player.prototype.update = function(){
 	if(this.x < 0){
 		this.x= 0;
 	}
-	if(player.alive && fire_button.isDown || game.input.activePointer.isDown) {
+	if(player.alive && fireButton.isDown || game.input.activePointer.isDown) {
 		this.fireBullet(bullets);
 	}
 };
 //This method handles bullet firing, can be used for different kinds of ammo
 Player.prototype.fireBullet = function(bullet_type){
+	var BULLET_SPEED = 150;
+	var BULLET_SPACING = 600;
 	// To avoid them being allowed to fire too fast we set a time limit
-	if (game.time.now > BULLET_TIMER){
+	if (game.time.now > bulletTimer){
 		//Grab the first bullet we can from the pool
 		var bullet = bullet_type.getFirstExists(false);
 		if (bullet){
@@ -81,9 +93,41 @@ Player.prototype.fireBullet = function(bullet_type){
 			bullet.body.velocity.x += BULLET_SPEED;
 			laser.play();
 			//Increase Timer
-			BULLET_TIMER = game.time.now + BULLET_SPACING;
+			bulletTimer = game.time.now + BULLET_SPACING;
 		}
 	}
+};
+//Collision between player and enemy ships
+Player.prototype.shipCollide = function(player, enemy){
+ 	//Play sound
+	shield_down.play();
+	//Add and play animation
+	var explosion = game.add.sprite(enemy.x, enemy.y, 'explosion');
+	explosion.anchor.setTo(0.5, 0.5);
+	explosion.animations.add('boom');
+	explosion.play('boom', 15, false, true);
+	//Decrease Health
+	this.damage(20);
+	shields.render();
+	var text = game.add.text(this.x, this.y - 40, '-20%', textOptions);
+	text.anchor.set(0.5);
+	game.add.tween(text).to({alpha: 0}, 2000, Phaser.Easing.Linear.None, true);
+	//Remove enemy
+	enemy.kill();
+};
+//Collision between enemies and bullets
+Player.prototype.collectPower = function(player, power){
+	//Play sound
+	power_up.play();
+	//Text Popup
+	var text = game.add.text(power.x, power.y -20, '+20%', textOptions);
+	text.anchor.set(0.5);
+	game.add.tween(text).to({alpha: 0}, 2000, Phaser.Easing.Linear.None, true);
+	//Remove objects
+	power.kill();
+	//Increase score
+	this.health +=20;
+	shields.render();
 };
 
 //Bullet class
@@ -100,24 +144,6 @@ var BulletGroup = function(game){
 };
 //Extends phaser Group class
 BulletGroup.prototype = Object.create(Phaser.Group.prototype);
-BulletGroup.prototype.constructor = BulletGroup;
-
-//PowerUp class
-var PowerUpGroup = function(game, sprite){
-	Phaser.Group.call(this, game);
-	this.enableBody = true;
-	this.physicsBodyType = Phaser.Physics.ARCADE;
-	this.createMultiple(3, sprite);
-	this.setAll('anchor.x', -1.5);
-	this.setAll('anchor.y', -5);
-	this.setAll('outOfBoundsKill', true);
-	this.setAll('checkWorldBounds', true);
-	game.add.group();
-};
-//Extends phaser Group class
-PowerUpGroup.prototype = Object.create(Phaser.Group.prototype);
-PowerUpGroup.prototype.constructor = BulletGroup;
-
 
 //Enemy class
 var EnemyGroup = function(game, sprite){
@@ -130,10 +156,119 @@ var EnemyGroup = function(game, sprite){
 	this.setAll('anchor.y', 0.5);
 	this.setAll('outOfBoundsKill', true);
 	this.setAll('checkWorldBounds', true);
-	};
+};
 //Extends phaser Group class
 EnemyGroup.prototype = Object.create(Phaser.Group.prototype);
-EnemyGroup.prototype.constructor = EnemyGroup;
+//This function handles the spawn of the enemy wave, the eventTimer recursively recalls the function
+//after a randomly generated delay
+EnemyGroup.prototype.launchEnemy = function(){
+	var MIN_SPACING = 550;
+	var MAX_SPACING = 3000;
+	var SPEED = -240;
+	//Takes enemy from pool and launches it
+	var enemy = this.getFirstExists(false);
+	if(enemy) {
+		enemy.reset(820, game.rnd.integerInRange(25, 480));
+		enemy.body.velocity.x = SPEED;
+		enemy.body.velocity.y = game.rnd.integerInRange(0, 300);
+		enemy.body.drag.y = game.height/2;
+		//Single enemy instance update method
+		enemy.update = function(){
+			//Kill enemies once they go off screen
+			if(enemy.x < -200) {
+				enemy.kill();
+			}
+		};
+	}
+	//Event Timer
+	enemyTimer = game.time.events.add(game.rnd.integerInRange(MIN_SPACING, MAX_SPACING), this.launchEnemy, this);
+};
+//This function handles the spawn of the ufo wave, the eventTimer recursively recalls the function
+//after a fixed delay
+EnemyGroup.prototype.launchUfo = function(){
+	var START_SPD = game.rnd.integerInRange(25, 480);
+	var SPEED = -120;
+	var SPREAD = 60;
+	var FREQUENCY = 70;
+	var ENEMY_SPACING = 6000;
+	//Launch wave
+	var ufo = this.getFirstExists(false);
+	if (ufo) {
+		this.START_SPD = START_SPD;
+		ufo.reset(820, this.START_SPD);
+		ufo.body.velocity.x = SPEED;
+		//Update function for each enemy
+		ufo.update = function(){
+			//Wave movement
+			this.body.y = Math.sin((this.x) / FREQUENCY) * SPREAD + START_SPD;
+			//Kill enemies once they go off screen
+			if(ufo.x < -200) {
+				ufo.kill();
+			}
+		};
+	}
+	//Event Timer
+	ufoTimer = game.time.events.add(ENEMY_SPACING, this.launchUfo, this);
+};
+//Collision between enemies and bullets
+EnemyGroup.prototype.hitEnemy = function(enemy, bullet){
+	//Play sound
+	explode.play();
+	//Add and play animation
+	var explosion = game.add.sprite(enemy.x, enemy.y, 'explosion');
+	explosion.anchor.setTo(0.5, 0.5);
+	explosion.animations.add('boom');
+	explosion.play('boom', 15, false, true);
+	//Text Popup
+	var text = game.add.text(enemy.x, enemy.y - 40, '+20', textOptions);
+	text.anchor.set(0.5);
+	game.add.tween(text).to({alpha: 0}, 2000, Phaser.Easing.Linear.None, true);
+	//Remove objects
+	enemy.kill();
+	bullet.kill();
+	//Increase score
+	score += 20;
+	scoreText.render();
+};
+
+//PowerUp class
+var PowerUpGroup = function(game, sprite){
+	Phaser.Group.call(this, game);
+	game.add.group();
+	this.enableBody = true;
+	this.physicsBodyType = Phaser.Physics.ARCADE;
+	this.createMultiple(5, sprite);
+	this.setAll('anchor.x', 0.5);
+	this.setAll('anchor.y', 0.5);
+	this.setAll('outOfBoundsKill', true);
+	this.setAll('checkWorldBounds', true);
+	};
+//Extends phaser Group class
+PowerUpGroup.prototype = Object.create(Phaser.Group.prototype);
+//This function handles the spawn of the powerUP wave, the eventTimer recursively recalls the function
+//after a randomly generated delay
+PowerUpGroup.prototype.launchPowerShield = function(){
+	var MIN_SPACING = 20000;
+	var MAX_SPACING = 40000;
+	var SPEED = -140;
+	//Takes enemy from pool and launches it
+	var power_up = powerShield.getFirstExists(false);
+	if(power_up) {
+		power_up.reset(game.rnd.integerInRange(game.width/3, game.width-25), game.rnd.integerInRange(25, game.height - 25));
+		power_up.body.velocity.x = SPEED;
+		//Single powerUP instance update method
+		power_up.update = function(){
+			//Kill powerUP once they go off screen
+			if(power_up.x < -200) {
+				power_up.kill();
+			}
+		};
+	}
+	//Event Timer
+	powerTimer = game.time.events.add(game.rnd.integerInRange(MIN_SPACING, MAX_SPACING), this.launchPowerShield, this);
+
+};
+
 
 /*
 *
@@ -148,10 +283,13 @@ function preload(){
 	game.load.image('bullet', 'images/laser.png');
 	game.load.image('enemy', 'images/enemy.png');
 	game.load.image('ufo', 'images/ufo.png');
+	game.load.image('powerShield', 'images/power2.png');
 	game.load.spritesheet('explosion', 'images/explosion.png', 128, 128);
 	game.load.audio('laser', 'images/laser2.ogg');
 	game.load.audio('loop', 'images/loop.mp3');
 	game.load.audio('explode', 'images/explosion.ogg');
+	game.load.audio('s_down', 'images/shield_down.ogg');
+	game.load.audio('power_up', 'images/power_up.ogg');
 }
 
 //This function is used to instantiate game entities
@@ -160,20 +298,24 @@ function create(){
 	player = new Player(game);
 	laser = game.add.audio('laser');
 	explode = game.add.audio('explode');
-	loop = game.add.audio('loop');
-	loop.loopFull();
+	shield_down = game.add.audio('s_down');
+	power_up = game.add.audio('power_up');
+	background_music = game.add.audio('loop');
+	background_music.loopFull();
 	bullets = new BulletGroup(game);
 	enemies = new EnemyGroup(game, 'enemy');
-	ufo = new EnemyGroup(game, 'ufo');
-	//Creates first wave event, 1 sec delay
-	game.time.events.add(1000, launchEnemy);
-	game.time.events.add(1000, launchUfo);
-	//Adds player Health on screen
+	ufos = new EnemyGroup(game, 'ufo');
+	powerShield = new PowerUpGroup(game, 'powerShield');
+	//Create events
+	game.time.events.add(1000, enemies.launchEnemy, enemies);
+	game.time.events.add(1000, ufos.launchUfo, ufos);
+	game.time.events.add(10000, powerShield.launchPowerShield, powerShield);
+	//Add player Health on screen
 	shields = game.add.text(game.world.width - 150, 10, 'Health: ' + player.health +'%', { font: '20px Arial', fill: '#fff' });
 	shields.render = function() {
 		shields.text = 'Shields: ' + Math.max(player.health, 0) +'%';
 	};
-	//Adds Score on screen
+	//Add Score on screen
 	scoreText = game.add.text(10, 10, '', { font: '20px Arial', fill: '#fff' });
 	scoreText.render = function() {
 		scoreText.text = 'Score: ' + score;
@@ -185,7 +327,7 @@ function create(){
 	gameOver.visible = false;
 	//Controls
 	cursors = game.input.keyboard.createCursorKeys();
-	fire_button = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+	fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 //This Function updates the game
@@ -193,16 +335,17 @@ function update(){
 	//Scroll the background
 	starfield.tilePosition.x -= 1.5;
 	//Check collisions
-	game.physics.arcade.overlap(player, enemies, shipCollide, null, this);
-	game.physics.arcade.overlap(player, ufo, shipCollide, null, this);
-	game.physics.arcade.overlap(enemies, bullets, hitEnemy, null, this);
-	game.physics.arcade.overlap(ufo, bullets, hitEnemy, null, this);
+	game.physics.arcade.overlap(player, enemies, player.shipCollide, null, player);
+	game.physics.arcade.overlap(player, ufos, player.shipCollide, null, player);
+	game.physics.arcade.overlap(enemies, bullets, enemies.hitEnemy, null, enemies);
+	game.physics.arcade.overlap(ufos, bullets, ufos.hitEnemy, null, ufos);
+	game.physics.arcade.overlap(player, powerShield, player.collectPower, null, player);
 	//Game over?
 	if(!player.alive && gameOver.visible === false){
 		var setResetHandlers = function() {
 			//The "click to restart" handler
 			tapRestart = game.input.onTap.addOnce(_restart,this);
-			spaceRestart = fire_button.onDown.addOnce(_restart,this);
+			spaceRestart =  fireButton.onDown.addOnce(_restart,this);
 			function _restart() {
 			tapRestart.detach();
 			spaceRestart.detach();
@@ -215,14 +358,17 @@ function update(){
 		fadeInGameOver.onComplete.add(setResetHandlers);
 		fadeInGameOver.start();
 	}
- }
+}
 
 //This function restarts the game after GameOver
 function restart(){
 	//Reset the enemies
 	enemies.callAll('kill');
+	ufos.callAll('kill');
 	game.time.events.removeAll();
-	game.time.events.add(1000, launchEnemy);
+	game.time.events.add(1000, enemies.launchEnemy, enemies);
+	game.time.events.add(1000, ufos.launchUfo, ufos);
+	game.time.events.add(10000, powerShield.launchPowerShield, powerShield);
 	//Revive the player
 	player.revive();
 	player.health = 100;
@@ -234,99 +380,4 @@ function restart(){
 }
 
 function render(){
-  }
-
-/*
-*
-*OTHER FUNCTIONS
-*
-*/
-
-//This function handles the spawn of the first enemy wave, the eventTimer recursively recalls the function
-//after a randomly generated delay
-function launchEnemy(){
-	var MIN_ENEMY_SPACING = 550;
-	var MAX_ENEMY_SPACING = 3000;
-	var ENEMY_SPEED = 300;
-	//Takes enemy from pool and launches it
-	var enemy = enemies.getFirstExists(false);
-	if(enemy) {
-		enemy.reset(820, game.rnd.integerInRange(25, 480));
-		enemy.body.velocity.x = -240;
-		enemy.body.velocity.y = game.rnd.integerInRange(0, 300);
-		enemy.body.drag.y = game.height/2;
-	}
-	//Event Timer
-	enemyTimer = game.time.events.add(game.rnd.integerInRange(MIN_ENEMY_SPACING, MAX_ENEMY_SPACING), launchEnemy);
-	//Single enemy instance update method
-	enemy.update = function(){
-		//Kill enemies once they go off screen
-		if(enemy.x < -200) {
-		enemy.kill();
-		enemy.x = -20;
-		}
-	};
-}
-
-//This function handles the spawn of the first ufo wave, the eventTimer recursively recalls the function
-//after a fixed delay
-function launchUfo(){
-	var startingspd = game.rnd.integerInRange(25, 480);
-	var verticalSpeed = 120;
-	var spread = 60;
-	var frequency = 70;
-	var timeBetweenWaves = 6000;
-	var startingspd;
-	//Launch wave
-	var enemy = ufo.getFirstExists(false);
-	if (enemy) {
-		this.startingspd = startingspd;
-		enemy.reset(820, this.startingspd);
-		enemy.body.velocity.x = -verticalSpeed;
-		//Update function for each enemy
-		enemy.update = function(){
-			//Wave movement
-			this.body.y = Math.sin((this.x) / frequency) * spread + startingspd;
-			//Kill enemies once they go off screen
-			if (this.y > game.height + 200) {
-			this.kill();
-			}
-		}
-	}
-
-   //  Send another wave soon
-   ufoTimer = game.time.events.add(timeBetweenWaves, launchUfo);
-}
-
-//Collision between player and enemy ships
-function shipCollide(player, enemy){
- 	//Play sound
-	explode.play();
-	//Add and play animation
-	var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
-	explosion.anchor.setTo(0.5, 0.5);
-	explosion.animations.add('boom');
-	explosion.play('boom', 15, false, true);
-	//Decrease Health
-	player.damage(20);
-	shields.render();
-	//Remove enemy
-	enemy.kill();
-}
-
-//Collision between enemies and bullets
-function hitEnemy(enemy, bullet){
-	//Play sound
-	explode.play();
-	//Add and play animation
-	var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
-	explosion.anchor.setTo(0.5, 0.5);
-	explosion.animations.add('boom');
-	explosion.play('boom', 15, false, true);
-	//Remove objects
-	enemy.kill();
-	bullet.kill();
-	//Increase score
-	score += 20;
-	scoreText.render();
 }
